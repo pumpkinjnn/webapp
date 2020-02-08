ce6a42c36fed90d6b0d9e87d72e3f1ed7b9b50a1
Nannan Ji <jinannan@grinnell.edu>
2020-02-08 02:40:29 -0600

    hw3

---
+++ app/controllers/welcome_controller.rb
@@ -0,0 +1,14 @@
+class WelcomeController < ApplicationController
+  def homepage
+  end
+
+  def index
+  end
+
+  def generic
+  end
+
+  def elements
+  end
+
+end
---
+++ config/routes.rb
@@ -0,0 +1,13 @@
+Rails.application.routes.draw do
+  get 'welcome/homepage'
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'welcome#homepage'
+
+  get 'welcome/index'
+
+  get 'welcome/generic'
+
+  get 'welcome/elements'
+
+end
